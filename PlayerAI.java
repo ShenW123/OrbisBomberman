5d7c018ea8070a85b4aab88d77723a88a9262a8c
Vivian Liu <viv.liu@mail.utoronto.ca>
2013-09-22 02:22:26 -0400

    Not working search iterations

--- PlayerAI.java
+++ PlayerAI.java
@@ -22,9 +22,8 @@ public class PlayerAI implements Player {
     public static final int BLOCK_V = -3;
     private MapItems[][] map = new MapItems[16][16];
     List<Point> allBlocks;
-    LinkedList<Move.Direction> movesSequence;
     int[][] heightMap = new int[17][17];
-
+    private Point goalPoint;
     /**
      * Gets called every time a new game starts.
      *
@@ -36,7 +35,6 @@ public class PlayerAI implements Player {
     @Override
     public void newGame(MapItems[][] map, List<Point> blocks, Bomber[] players, int playerIndex) {
         allBlocks = blocks;
-        movesSequence = new LinkedList<>();
         String temp="";
         for (int i = 1; i < 16; i++) {
             for (int j = 1; j < 16; j++) {
@@ -78,9 +76,6 @@ public class PlayerAI implements Player {
 
         this.map = map;
 
-        for (Point p : allBlocks) {
-            updateHeightMapBlocks(p);
-        }
         for (Map.Entry entry : bombLocations.entrySet()) { 
             Point point = (Point) entry.getKey();
             Bomb bomb = (Bomb) entry.getValue();
@@ -88,12 +83,15 @@ public class PlayerAI implements Player {
         }
 
         for(Point p: explosionLocations) {
-            cleanBomb(p);
             if (allBlocks.contains(p)) {
                 allBlocks.remove(p);
             }
+            updateHeightMapExplosions(p);
         }
 
+        for (Point p : allBlocks) {
+            updateHeightMapBlocks(p);
+        }
         String temp="";
         for (int i = 1; i < 16; i++) {
             for (int j = 1; j < 16; j++) {
@@ -102,12 +100,16 @@ public class PlayerAI implements Player {
             System.out.println(temp);
             temp = "";
         }
+
         boolean bombMove = false;
         /**
          * Get Bomber's current position
          */
         Point curPosition = players[playerIndex].position;
+        LinkedList<Point> path = findGoal(curPosition, map);
         int minScore = 100;
+        int minX = 100;
+        int minY = 100;
         Move.Direction bestMove = new Move.Direction(0, 0, PlayerAction.STAYPUT, PlayerAction.PLACEBOMB);
 
         /**
@@ -122,14 +124,20 @@ public class PlayerAI implements Player {
 
             if (map[x][y].isWalkable()) {
                 // TODO: hardcoded 14.
-                if (heightMap[x][y] < 14){
-                    if(minScore > heightMap[x][y]) {
-                        minScore = heightMap[x][y];
+                if(!path.isEmpty()) {
+                    Point suggested = path.pop();
+                    if(x == suggested.x && y == suggested.y) {
                         bestMove = move;
-                    }
-                    validMoves.add(move);
-                    if(heightMap[curPosition.x][curPosition.y] == -3) {
-                        bombMove = true;
+                        System.out.println("BestMove = " + suggested);
+                    } else if (heightMap[x][y] < 14){
+                        if(minScore > heightMap[x][y]) {
+                            minScore = heightMap[x][y];
+                            bestMove = move;
+                        }
+                        validMoves.add(move);
+                        if(heightMap[curPosition.x][curPosition.y] == -3) {
+                            bombMove = true;
+                        }
                     }
                 }
             }
@@ -137,14 +145,6 @@ public class PlayerAI implements Player {
                 blocks.add(move);
             }
         }
-
-        /**
-         * If there are blocks around, I should place a bomb in my current square.
-         */
-        if (!blocks.isEmpty()) {
-            bombMove = true;
-        }
-
         /**
          * There's no place to go, I'm stuck. :(
          */
@@ -153,19 +153,47 @@ public class PlayerAI implements Player {
             return Move.still.action;
         }
 
+        for(Point p: explosionLocations) {
+            cleanBomb(p);
+        }
         /**
          * If I've got moves stored in movesSequence, use the oldest one in there (index 0). 
          * Else pick the first validMove available lol
          */
         //Move.Direction move = validMoves.get((int) (Math.random() * validMoves.size()));
-        if (bombMove) {
+        /*if (bombMove) {
             return bestMove.bombaction;
-        }
+        }*/
         System.out.println(bestMove.action); 
         return bestMove.action;
 
     }
 
+    private boolean hasGetAway(Point p) {
+        int x = p.x;
+        int y = p.y;
+        if(map[x][y].isWalkable()) {
+            if(map[x-1][y].isWalkable() && heightMap[x-1][y] <= 0) {
+                return true;
+            }
+            if(map[x+1][y].isWalkable() && heightMap[x+1][y] <= 0) {
+                return true;
+            }
+            if(map[x][y-1].isWalkable() &&heightMap[x][y-1] <= 0) {
+                return true;
+            }
+            if(map[x][y+1].isWalkable() &&heightMap[x][y+1] <= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updateHeightMapExplosions(Point p) {
+        int x = p.x;
+        int y = p.y;
+        heightMap[x][y] = 50;
+    }
     private void updateHeightMapBlocks(Point p) {
         int x = p.x;
         int y = p.y;
@@ -235,27 +263,31 @@ public class PlayerAI implements Player {
      * @param map The map use to check if a path exists between point A and point B
      * @return True if there is a walkable path between point A and point B, False otherwise.
      */
-    public boolean isThereAPath(Point start, Point end, MapItems[][] map) {
+    public LinkedList<Point> findGoal(Point start, MapItems[][] map) {
         //Keeps track of points we have to check
-        Queue<Point> open = new LinkedList<>();
+        Queue<Node> open = new LinkedList<>();
 
         //Keeps track of points we have already visited
-        List<Point> visited = new LinkedList<>();
+        LinkedList<Node> visited = new LinkedList<>();
 
-        open.add(start);
+        LinkedList<Point> path = new LinkedList<>();
+        path.add(start);
+        open.add(new Node(null, start));
+        Node neighbour = new Node(null, start);
+        lala:
         while (!open.isEmpty()) {
-            Point curPoint = open.remove();
+            Node curNode = open.remove();
 
             //Check all the neighbours of the current point in question
             for (Move.Direction direction : Move.getAllMovingMoves()) {
-                int x = curPoint.x + direction.dx;
-                int y = curPoint.y + direction.dy;
+                int x = curNode.getPoint().x + direction.dx;
+                int y = curNode.getPoint().y + direction.dy;
 
-                Point neighbour = new Point(x, y);
+                neighbour = new Node(curNode, new Point (x, y));
 
                 // if the point is the destination, we are done
-                if (end.equals(neighbour)) {
-                    return true;
+                if (map[neighbour.getPoint().x][neighbour.getPoint().y].isWalkable() && heightMap[neighbour.getPoint().x][neighbour.getPoint().y] <= -3) {
+                    break lala;
                 }
 
                 // if we have already visited this point, we skip it
@@ -272,9 +304,36 @@ public class PlayerAI implements Player {
                 visited.add(neighbour);
             }
         }
-        return false;
+        if(!visited.isEmpty()) {
+            while (neighbour.getParent() != null) {
+                path.add(neighbour.getPoint());
+                System.out.println("Path " + neighbour.getPoint()); 
+                neighbour = neighbour.getParent();
+    /*            visited.pop();*/
+                
+            } 
+        }
+
+        return path;
     }
 
+    public class Node {
+        private Node mParent;
+        private Point mPoint;
+        public Node (Node parent, Point p) {
+            mParent = parent;
+            mPoint = p;
+        }
+
+        public Node getParent() {
+            return mParent;
+        }
+
+        public Point getPoint() {
+            return mPoint;
+        }
+
+    }
     /**
      * Returns the Manhattan Distance between the two points.
      *
